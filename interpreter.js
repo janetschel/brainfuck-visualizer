7e3540f3db0c6c9da510e0f4299ac029766512ee
Jan Etschel <etschelj@gmail.com>
2021-05-26 12:12:10 +0200

    init + base code

---
+++ interpreter.js
@@ -0,0 +1,86 @@
+const output = document.getElementById("output");
+
+const interpret = () => {
+    const code = document.getElementById("brainfuck-code").value.split("");
+    const matchingParenthese = findMatchingParenthese(code);
+
+    output.innerHTML = "";
+
+    let tape = {};
+    let pointer = 0;
+
+    for (let index = 0; index < code.length; index++) {
+        const currentCharacter = code[index];
+
+        if (["<", ">"].includes(currentCharacter)) {
+            pointer += currentCharacter == "<" ? -1 : 1;
+        } else if (["+", "-"].includes(currentCharacter)) {
+            tape[`${pointer}`] = existsOnTapeOrElse(pointer, tape, 0) + (currentCharacter === "-" ? -1 : 1);
+            checkForWrap(pointer, tape);
+        }  else if (currentCharacter === ".") {
+            output.innerHTML += String.fromCharCode(tape[`${pointer}`]);
+        } else if (currentCharacter === ",") {
+            const number = prompt("Type your input:", "");
+            tape[`${pointer}`] = parseInt(number);
+        } else if ((currentCharacter === "[" && tape[`${pointer}`] === 0) || (currentCharacter === "]" && tape[`${pointer}`] !== 0)) {
+            index = findMatchingParentheseForIndex(matchingParenthese, index);
+        }
+    }
+
+    console.log(tape)
+};
+
+const findMatchingParenthese = (code) => {
+    let matching = {};
+
+    for (let index = 0; index < code.length; index++) {
+        const currentCharacter = code[index];
+        
+        if (currentCharacter === "[") {
+            const closingIndex = findMatchingForSpecificParenthese(index, code);
+            matching[`${index}`] = closingIndex;
+        }
+    }
+
+    return matching;
+};
+
+const findMatchingForSpecificParenthese = (index, code) => {
+    let countOfNonRelated = 0;
+
+    for (let j = index + 1; j < code.length; j++) {
+        const currentCharacter = code[j];
+
+        if (currentCharacter === "[") {
+            countOfNonRelated++;
+        } else if (currentCharacter === "]" && countOfNonRelated > 0) {
+            countOfNonRelated--;
+        } else if (currentCharacter === "]" && countOfNonRelated === 0) {
+            return j;
+        }
+    }
+};
+
+const findMatchingParentheseForIndex = (matchingParenthese, findFor) => {
+    if (findFor in matchingParenthese) {
+        return matchingParenthese[findFor];
+    }
+
+    for (const [key, value] of Object.entries(matchingParenthese)) {
+        if (value === findFor) {
+            return parseInt(key);
+        }
+    }
+};
+
+const checkForWrap = (pointer, tape) => {
+    if (tape[`${pointer}`] > 255) {
+        tape[`${pointer}`] = 0;
+    } else if (tape[`${pointer}`] < 0) {
+        tape[`${pointer}`] = 255;
+    }
+};
+
+const existsOnTapeOrElse = (key, tape, elseValue) => {
+    return key in tape ? tape[`${key}`] : elseValue;
+};
